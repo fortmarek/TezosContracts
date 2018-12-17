956f6c9287ff98d506edd2ad7799980c5bd2f009
Marek Fořt <marekfort@me.com>
2018-12-17 08:51:16 +0100

    Initial Commit

---
+++ TezosContracts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TezosContracts
+//
+//  Created by Marek Fořt on 12/17/18.
+//  Copyright © 2018 Marek Fořt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
