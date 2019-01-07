44fa61f00fc28f53bb439a680cbca8c376396d52
Marek Fořt <marek.fort@ackee.cz>
2019-01-07 14:26:55 +0100

    Follow part one.

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '10.0'
+
+inhibit_all_warnings!
+use_frameworks!
+
+target 'TezosContracts' do
+    pod 'TezosSwift', '~> 0.1'
+end
--- TezosContracts/ViewController.swift
+++ TezosContracts/ViewController.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import TezosSwift 
 
 class ViewController: UIViewController {
 
+    let tezosClient = TezosClient(remoteNodeURL: URL(string: "https://rpcalpha.tzbeta.net/")!)
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tezosClient.balance(of: "KT1DwASQY1uTEkzWUShbeQJfKpBdb2ugsE5k", completion: { result in
+            switch result {
+            case .success(let balance):
+                print(balance.humanReadableRepresentation)
+            case .failure(let error):
+                print("Getting balance failed with error: \(error)")
+            }
+        })
+
+        let mnemonic = "soccer click number muscle police corn couch bitter gorilla camp camera shove expire pill praise"
+        let wallet = Wallet(mnemonic: mnemonic)!
+
+        tezosClient.send(amount: Tez(1), to: "tz1WRFiK6eGNvP3ioWkWeP6JwDaQjj95opnQ", from: wallet, completion: { result in
+            switch result {
+            case .success(let transactionHash):
+                print(transactionHash)
+            case .failure(let error):
+                print("Sending Tezos failed with error: \(error)")
+            }
+        })
     }
 
 
